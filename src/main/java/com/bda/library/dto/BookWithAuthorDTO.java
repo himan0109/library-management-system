7a62c0f9dc1cc9501abf193b5d4aea640a0db636
himan2005 <scidutta2005@gmail.com>
2026-05-03 02:21:49 +0530

    Fix: remove Lombok from BookWithAuthorDTO

--- src/main/java/com/bda/library/dto/BookWithAuthorDTO.java
+++ src/main/java/com/bda/library/dto/BookWithAuthorDTO.java
@@ -1,12 +1,5 @@
 package com.bda.library.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class BookWithAuthorDTO {
     private Long bookId;
     private String title;
@@ -16,4 +9,42 @@ public class BookWithAuthorDTO {
     private Long authorId;
     private String authorName;
     private String authorNationality;
+
+    public BookWithAuthorDTO() {}
+
+    public BookWithAuthorDTO(Long bookId, String title, String isbn, int publishedYear,
+                             String genre, Long authorId, String authorName, String authorNationality) {
+        this.bookId = bookId;
+        this.title = title;
+        this.isbn = isbn;
+        this.publishedYear = publishedYear;
+        this.genre = genre;
+        this.authorId = authorId;
+        this.authorName = authorName;
+        this.authorNationality = authorNationality;
+    }
+
+    public Long getBookId() { return bookId; }
+    public void setBookId(Long bookId) { this.bookId = bookId; }
+
+    public String getTitle() { return title; }
+    public void setTitle(String title) { this.title = title; }
+
+    public String getIsbn() { return isbn; }
+    public void setIsbn(String isbn) { this.isbn = isbn; }
+
+    public int getPublishedYear() { return publishedYear; }
+    public void setPublishedYear(int publishedYear) { this.publishedYear = publishedYear; }
+
+    public String getGenre() { return genre; }
+    public void setGenre(String genre) { this.genre = genre; }
+
+    public Long getAuthorId() { return authorId; }
+    public void setAuthorId(Long authorId) { this.authorId = authorId; }
+
+    public String getAuthorName() { return authorName; }
+    public void setAuthorName(String authorName) { this.authorName = authorName; }
+
+    public String getAuthorNationality() { return authorNationality; }
+    public void setAuthorNationality(String authorNationality) { this.authorNationality = authorNationality; }
 }
